b8ceae70752898db4f10ae4c31a067496cde4655
helpceo <helpceo@126.com>
2018-07-03 17:20:48 +0800

    first commit

---
+++ app/components/Home.js
@@ -0,0 +1,43 @@
+// @flow
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.css';
+import { Button, Switch } from 'antd';
+
+const {ipcRenderer} = window.require('electron')
+
+type Props = {};
+
+export default class Home extends Component<Props> {
+  props: Props;
+  constructor() {
+    super();
+    this.state = {
+      value: "不好看",
+      age: 10
+    };
+  }
+  handleClick = function() {
+    ipcRenderer.on('asynchronous-reply', (event, arg) => {
+      console.log(arg)
+        this.setState({
+          value:arg
+        })
+        
+    })
+    ipcRenderer.send('asynchronous-message', 'true')
+  }
+  render() {
+    return (
+      <div>
+        <div className={styles.container} data-tid="container">
+          <h2>Home</h2>
+          <Button type="primary" onClick={this.handleClick.bind(this)}>点击</Button>
+          <Switch checkedChildren="开" unCheckedChildren="关" defaultChecked />
+          <Link to="/counter">{this.state.value}</Link>
+          <p>这是热更新</p>
+        </div>
+      </div>
+    );
+  }
+}
